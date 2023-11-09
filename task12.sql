ce5e490b9286e5d53c447d9a4fc2f7f6aca21933
Tech-Shark <118375376+Tech-Shark@users.noreply.github.com>
2023-11-09 19:28:20 +0400

    Update task12.sql

--- task12.sql
+++ task12.sql
@@ -30,38 +30,6 @@ ADD CONSTRAINT FK_Manager
 FOREIGN KEY (ManagerID)
 REFERENCES Managers(ManagerID);
 
--- Employees Table
-CREATE TABLE Employees (
-    EmployeeID INT PRIMARY KEY,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    DepartmentID INT,
-    ManagerID INT
-);
-
--- Departments Table
-CREATE TABLE Departments (
-    DepartmentID INT PRIMARY KEY,
-    DepartmentName VARCHAR(100)
-);
-
--- Managers Table
-CREATE TABLE Managers (
-    ManagerID INT PRIMARY KEY,
-    ManagerName VARCHAR(50)
-);
-
--- Create Foreign Key Relationships
-ALTER TABLE Employees
-ADD CONSTRAINT FK_Department
-FOREIGN KEY (DepartmentID)
-REFERENCES Departments(DepartmentID);
-
-ALTER TABLE Employees
-ADD CONSTRAINT FK_Manager
-FOREIGN KEY (ManagerID)
-REFERENCES Managers(ManagerID);
-
 
 SELECT e.FirstName, e.LastName, d.DepartmentName
 FROM Employees e
